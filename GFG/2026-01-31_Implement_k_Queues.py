c4a2af92a26a4ee5b916a37387f283f5a1467535
Saurabh <sauravzoner@gmail.com>
2026-01-31 12:39:03 +0530

    Daily practice

---
+++ GFG/2026-01-31_Implement_k_Queues.py
@@ -0,0 +1,39 @@
+class kQueues:
+    def __init__(self, n, k):
+        self.n = n
+        self.k = k
+        self.arr = [0] * n
+        self.front = [-1] * k
+        self.rear = [-1] * k
+        self.next = list(range(1, n)) + [-1]
+        self.free = 0
+
+    def enqueue(self, x, i):
+        if self.free == -1:
+            return
+        idx = self.free
+        self.free = self.next[idx]
+        if self.front[i] == -1:
+            self.front[i] = idx
+        else:
+            self.next[self.rear[i]] = idx
+        self.next[idx] = -1
+        self.rear[i] = idx
+        self.arr[idx] = x
+
+    def dequeue(self, i):
+        if self.front[i] == -1:
+            return -1
+        idx = self.front[i]
+        self.front[i] = self.next[idx]
+        self.next[idx] = self.free
+        self.free = idx
+        if self.front[i] == -1:
+            self.rear[i] = -1
+        return self.arr[idx]
+
+    def isEmpty(self, i):
+        return self.front[i] == -1
+
+    def isFull(self):
+        return self.free == -1
