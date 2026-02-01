af6bc5863981235d38dd6fd28aa06eec65067991
Saurabh <sauravzoner@gmail.com>
2026-02-01 11:41:41 +0530

    Daily practice

---
+++ LeetCode/2026-02-01_Divide_an_array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minimumCost(self, nums):
+        n = len(nums)
+        ans = float('inf')
+
+        for i in range(1, n-1):
+            for j in range(i+1, n):
+                cost = nums[0] + nums[i] + nums[j]
+                ans = min(ans, cost)
+
+        return ans
+
+        
