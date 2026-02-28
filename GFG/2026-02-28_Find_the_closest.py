06b5981e7474827f70fadc62b35a82e00aed07b4
Saurabh <sauravzoner@gmail.com>
2026-02-28 10:48:01 +0530

    Daily practice

---
+++ GFG/2026-02-28_Find_the_closest.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findClosestPair(self, arr1, arr2, x):
+        i = 0
+        j = len(arr2) - 1
+        diff = float('inf')
+        res = (0, 0)
+        
+        while i < len(arr1) and j >= 0:
+            s = arr1[i] + arr2[j]
+            
+            if abs(s - x) < diff:
+                diff = abs(s - x)
+                res = (arr1[i], arr2[j])
+            
+            if s > x:
+                j -= 1
+            else:
+                i += 1
+        
+        return list(res)
