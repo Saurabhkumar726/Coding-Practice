e90ec81a6b8384f3037b7105dbdd7a61a04dfc7b
Saurabh <sauravzoner@gmail.com>
2026-06-05 19:36:57 +0530

    Daily practice

---
+++ GFG/2026-06-05_lexicographically-smallest-after-removing-k.py
@@ -0,0 +1,27 @@
+class Solution:
+    def lexicographicallySmallest(self, s, k):
+        n = len(s)
+        
+        if n & (n - 1) == 0:
+            k //= 2
+        else:
+            k *= 2
+        
+        if k >= n:
+            return -1
+        
+        stack = []
+        
+        for ch in s:
+            while k > 0 and stack and stack[-1] > ch:
+                stack.pop()
+                k -= 1
+            stack.append(ch)
+        
+        while k > 0:
+            stack.pop()
+            k -= 1
+        
+        ans = ''.join(stack)
+        
+        return ans if ans else -1
